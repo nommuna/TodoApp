9e5b6c78a1b0bd1a735d407f0054251563c3e993
Muna <nommuna@gmail.com>
2019-11-28 19:48:56 -0800

    Added some stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import { Typography, Grid } from '@material-ui/core';
 
 const App: React.FC = () => {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
+		<Grid container spacing={2}>
+			<Grid item xs={12}>
+				<Typography variant="h2" gutterBottom align="center">
+					TODO App
+				</Typography>
+			</Grid>
+			<Grid item xs={12}></Grid>
+		</Grid>
 	);
 };
 
