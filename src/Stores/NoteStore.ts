368327171682b9b77e5cb5da08c00ff1ee8f1112
Muna <nommuna@gmail.com>
2019-11-30 00:40:22 -0800

    adding some debugging tools

--- src/Stores/NoteStore.ts
+++ src/Stores/NoteStore.ts
@@ -1,30 +1,33 @@
-import { observable, action } from 'mobx';
-import {createContext} from 'react';
+import { observable, action, computed } from 'mobx';
+import { createContext } from 'react';
+import { enableLogging } from 'mobx-logger';
+
+// enableLogging();
 
 class NoteStore {
-    @observable userString = '';
-    @observable userList = [] as Array<string>;
+	@observable userString = '';
+	@observable userList = [] as Array<string>;
 
-    @action setUserString(s: string) {
-        this.userString = s;
-    }
+	@action setUserString(s: string) {
+		this.userString = s;
+	}
 
-    @action updateList() {
-        if(this.userString === '' && this.userList.length >= 0){
-            return;
-        } else {
-            this.userList.push(this.userString);
-        }
-    }
+	@action updateList() {
+		if (this.userString === '' && this.userList.length >= 0) {
+			return;
+		} else {
+			this.userList.push(this.userString);
+		}
+	}
 
-    @action getList(): Array<string> {
-        this.userString = '';
-        return this.userList;
-    }
+	@action getList(): Array<string> {
+		this.userString = '';
+		return this.userList;
+	}
 
-    @action removeItem(index: number) {
-        this.userList.splice(index,1);
-    }
+	@action removeItem(index: number) {
+		this.userList.splice(index, 1);
+	}
 }
 
 export const NoteStoreContext = createContext(new NoteStore());
